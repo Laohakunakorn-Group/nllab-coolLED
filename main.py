1b1eaab71ca42eea501338da32f7257469dbd458
Nadanai Laohakunakorn <nadanai910@gmail.com>
2021-09-17 11:57:25 +0100

    Initial commit with materials

---
+++ main.py
@@ -0,0 +1,33 @@
+import serial, atexit
+
+### Configure serial communication 
+PORTNAME = 'COM3' # set port name here
+ser = serial.Serial() 
+ser.baudrate = 9600
+ser.port = PORTNAME
+ser.bytesize = 8 
+ser.parity = 'N'
+ser.stopbits = 1
+ser.timeout = 0 # second
+
+
+def handle_exit():
+    ser.close()
+    print('Port closed')
+
+def main(ser):
+
+    # Open port
+    ser.open()
+    print('Port open')
+    # Make sure to close port on exit
+    atexit.register(handle_exit)
+
+    # here is the app running
+
+    CMD = 'CSF\n' # CSN turns on, CSF turns off 
+    ser.write(CMD.encode())
+    print("Write")
+
+if __name__=='__main__':
+    main(ser)
